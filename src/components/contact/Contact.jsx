1a77bc34a8dde5ee576f7f1ba187ccca43e9263c
Miguel Lutz <miguellutz31@gmail.com>
2022-09-15 12:59:46 +0200

    Contact section styling

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -14,21 +14,21 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <ImMail />
+            <ImMail className='contact__option-icon' />
             <h4>Email</h4>
             <h5>miguellutz31@gmail.com</h5>
             <a href="mailto:miguellutz31@gmail.com" target="_blank" rel="noreferrer">Send Email</a>
           </article>
 
           <article className="contact__option">
-            <BsLinkedin />
+            <BsLinkedin className='contact__option-icon' />
             <h4>Linkedin</h4>
             <h5>Miguel Lutz</h5>
             <a href="https://linkedin.com/in/miguellutz" target='_blank' rel="noreferrer">Contact</a>
           </article>
 
           <article className="contact__option">
-            <FaPhoneSquareAlt />
+            <FaPhoneSquareAlt className='contact__option-icon' />
             <h4>Phone</h4>
             <h5>+4915752008690</h5>
             <a href="https://api.whatsapp.com/send?phone+4915752008690" target='_blank' rel="noreferrer">Send Message</a>
@@ -40,7 +40,7 @@ const Contact = () => {
           <input type="text" name='name' placeholder="Full Name" required />
           <input type="email" name='email' placeholder='Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
-          <button type="submit" className='btn btn-primary'>Send Message</button>
+          <button type="submit" className='btn btn-primary '>Send Message</button>
         </form>
       </div>
     </section>
